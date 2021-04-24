55b815cb0f21bb6bbaf00bd0d00a48c73ea14ea7
Dhruv Rajesh <makerspace98@gmail.com>
2021-04-24 08:32:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Hackathon Project</title>
-    <link href="index.css" rel="stylesheet" type="text/css" />
-
-    <script>
-    function displayScore() {
-      var earthHour = document.forms["data"]["hourParticipation"].value;
-      var charity = document.forms["data"]["charityIncentives"].value;
-      var rooftop = document.forms["data"]["rooftopGardens"].value;
-      var solar = document.forms["data"]["solarPanels"].value;
-
-      var score = 0
-
-      if (earthHour == "yes") {
-        score ++;
-      }
-      if (charity == "yes") {
-        score ++;
-      }
-      if (rooftop == "yes") {
-        score ++;
-      }
-      if (solar == "yes") {
-        score ++;
-      }
-
-      alert(score);
-
-    }
-    </script>
-  </head>
-  <body>
-    <h1>Hackathon Project</h1>
-    <form id="data" onsubmit="return displayScore()">
-      <b> Environmental Participation </b> <br>
-      Have you participated in Earth Hour? <input type="text" name="hourParticipation"><br>
-      Have you partaken in environmental charity incentives? <input type="text" name="charityIncentives"><br>
-      <hr>
-      <b> Sustainability </b> <br>
-      Do you have rooftop gardens? <input type="text" name="rooftopGardens"></input><br>
-      Do you have solar panels? <input type="text" name="solarPanels"></input><br>
-      <input type="submit" value="Submit">
-    </form>
-
-  </body>
-</html>
-"""
+    return render_template("index.html")
 
 def run():
   app.run(host='0.0.0.0', port=8080)
@@ -66,4 +15,3 @@ def keep():
   t.start()
 
 keep()
-
